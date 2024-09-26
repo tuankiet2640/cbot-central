bcd738cfe771af775d1e9349a799b011326b31a5
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-09-26 21:43:55 +0700

    rewrite the c bot central because the generated was too messy

--- testfastAPI.py
+++ testfastAPI.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1:{
+        "firstName": "kiet",
+        "lastname": "bui",
+        "age":"23",
+        "email":"tuankiet2640@gmail.com"
+    }
+}
 @app.get("/")
 def index ():
     return {"name":"first data"}
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
